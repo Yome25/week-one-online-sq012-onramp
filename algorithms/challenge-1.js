c9bfc33eef7a2a1f466e00f3afb0a90d62a57dbb
yome <yomeekoko25@gmail.com>
2022-06-23 11:09:29 -0700

    my edit

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,16 @@
 function secondLargest(arr) {
-  
+    const sortedArr = arr.sort((a, b) => {
+        if (a < b) {
+          return -1;
+        } else if (a > b) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+    
+      const secondBiggest = sortedArr[sortedArr.length - 2];
+    
+      return secondBiggest;
 }
 module.exports = secondLargest
